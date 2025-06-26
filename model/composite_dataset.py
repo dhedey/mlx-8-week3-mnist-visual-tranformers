282f688f584aa4c005dbadb742eb31cbd7bc2501
nick-kuhn <nikolaskuhn@gmx.de>
2025-06-26 15:32:41 +0100

    Fixed incorrect sampling of random images

--- model/composite_dataset.py
+++ model/composite_dataset.py
@@ -27,7 +27,7 @@ class BesCombine(Dataset):
     self.all_labels = torch.cat((torch.zeros(1), ds.targets)).to(torch.long)
     # Create a deterministic map of indices, so __getitem__(i) is always the same 
     self.index_map = [
-       np.random.choice(range(1, self.ln + 1), self.h_patches * self.w_patches, replace=False) for _ in range(self.ln)]
+       np.random.choice(range(1, len(ds) + 1), self.h_patches * self.w_patches, replace=False) for _ in range(self.ln)]
     self.skip_map = np.random.binomial(1, p_skip, size=(self.ln, self.h_patches * self.w_patches))
 
   def __len__(self):
