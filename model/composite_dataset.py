91ea17ade365e80151ae5509d50422b3cf3a6a02
nick-kuhn <nikolaskuhn@gmx.de>
2025-06-26 16:07:24 +0100

    BesCombine now deterministic

--- model/composite_dataset.py
+++ model/composite_dataset.py
@@ -15,7 +15,7 @@ class BesCombine(Dataset):
     self.p_skip = p_skip
 
     self.tk = { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 's': 10}
-
+    gen = np.random.default_rng(42)
     data_folder = os.path.join(os.path.dirname(__file__), "datasets")
     ds = torchvision.datasets.MNIST(root=data_folder, train=train, download=True)
     self.ln = len(ds) if length is None else length
@@ -26,8 +26,8 @@ class BesCombine(Dataset):
     self.processed_images = normalizer(all_images)
     self.all_labels = torch.cat((torch.zeros(1), ds.targets)).to(torch.long)
     # Create a deterministic map of indices, so __getitem__(i) is always the same 
-    self.index_map = np.random.choice(range(1, len(ds) + 1), size = (self.ln, self.h_patches * self.w_patches), replace=True)
-    self.skip_map = np.random.binomial(1, self.p_skip, size=(self.ln, self.h_patches * self.w_patches))
+    self.index_map = gen.choice(range(1, len(ds) + 1), size = (self.ln, self.h_patches * self.w_patches), replace=True)
+    self.skip_map = gen.binomial(1, self.p_skip, size=(self.ln, self.h_patches * self.w_patches))
 
   def __len__(self):
     return self.ln
